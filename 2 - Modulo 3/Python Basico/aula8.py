2290d7950aa0b68723feb68fb0797647ac97aaf3
Gabriel Torelo <57268681+GabrielTorelo@users.noreply.github.com>
2024-05-14 10:54:30 -0300

    aula 37: operadores de comparacao

---
+++ 2 - Modulo 3/Python Basico/aula8.py
@@ -0,0 +1,32 @@
+# Maior
+eh_maior = 10 > 5
+print(f'Maior: {eh_maior}')
+
+# Maior ou igual
+eh_maior_igual = 10 >= 5
+print(f'Maior ou igual: {eh_maior_igual}')
+
+# Menor
+eh_menor = 5 < 10
+print(f'Menor: {eh_menor}')
+
+# Menor ou igual
+eh_menor_igual = 5 <= 10
+print(f'Menor ou igual: {eh_menor_igual}')
+
+# Igual
+eh_igual = 5 == 5
+print(f'Igual: {eh_igual}')
+
+# Diferente
+eh_diferente = 5 != 10
+print(f'Diferente: {eh_diferente}')
+
+# Pertence
+lista = [1, 2, 3, 4, 5]
+pertence = 3 in lista
+print(f'Pertence: {pertence}')
+
+# Não pertence
+nao_pertence = 6 not in lista
+print(f'Não pertence: {nao_pertence}')
