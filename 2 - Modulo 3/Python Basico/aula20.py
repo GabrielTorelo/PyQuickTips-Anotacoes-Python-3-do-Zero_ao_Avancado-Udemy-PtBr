4081ceebbc17d8bee64a225c4c65f0f3ccbd3535
Gabriel Torelo <57268681+GabrielTorelo@users.noreply.github.com>
2024-05-16 09:28:47 -0300

    aula 96: exemplo de desempacotamento em chamadas

---
+++ 2 - Modulo 3/Python Basico/aula20.py
@@ -0,0 +1,8 @@
+salas = [
+  # 0         1      2       3
+  ["Maria", "João", "Ana", "Carlos"], #0
+  ["Marcos", "Cintia", "Vitor", "Mariana", "João"], #1
+  ["Paulo", "Lucas", ("Joana", "Pedro"), "Maria", "Júlia"] #2
+]
+
+print(*salas, sep="\n")
